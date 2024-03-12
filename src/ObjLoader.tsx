faa8045f071ff0f417a6336f9d02dc2a4b6477e0
@vlehnhau <vlehnhau@students.uni-mainz.de>
2024-03-12 11:18:38 +0100

    same push mit objloader

--- src/ObjLoader.tsx
+++ src/ObjLoader.tsx
@@ -1,151 +1,137 @@
-export const loadObj = (gl: WebGL2RenderingContext, objFileContent: string, mtlFileContent: string) => {
+export interface Material {
+    shininess?: number;
+    ambient?: number[];
+    diffuse?: number[];
+    specular?: number[];
+    emissive?: number[];
+    opticalDensity?: number;
+    opacity?: number;
+    illum?: number;
+    transparency?: number;
+    transmissionFilter?: number[];
+    roughness?: number;
+    metalness?: number;
+    sheen?: number;
+    dispersion?: number;
+  }
   
-    const splitFileContent = objFileContent.split('\n');
-
-    const dataPerVertex = 3;
-    const dataPerVertexNormal = 3;
-    const dataPerTexCoord = 2;
-    const dataPerFaceIndex = 9;
-
-    let vertexCount = 0;
-    let vertexNormalCount = 0;
-    let texCoordCount = 0;
-    let faceIndicesCount = 0;
-    let materialCount = 0;
-
-    for (const line of splitFileContent) {
-        switch (line[0]) {
-            case 'v': 
-                vertexCount += 1;
-                break;
-            case 'vn': 
-                vertexNormalCount += 1;
-                break;
-            case 'vt': 
-                texCoordCount += 1;
-                break;
-            case 'f': 
-                faceIndicesCount += 1;
-                break;
-            case 'usemtl':
-                materialCount += 1;
-                break;
-        }
-    }
-
-    const vertexPositions: Array<number> = new Array(dataPerVertex       * vertexCount);
-    const vertexNormals:   Array<number> = new Array(dataPerVertexNormal * vertexNormalCount);
-    const texCoords:       Array<number> = new Array(dataPerTexCoord     * texCoordCount);
-    const faceIndices:     Array<number> = new Array(dataPerFaceIndex    * faceIndicesCount);
-
-    vertexCount = 0;
-    vertexNormalCount = 0;
-    texCoordCount = 0;
-    faceIndicesCount = 0;
-    
-    for (const line of splitFileContent) {
-        const lineSplit = line.split(' ');
-        switch (line[0]) {
-            case 'v': {
-                for (let i = 0; i < dataPerVertex; i++) 
-                    vertexPositions[vertexCount + i] = parseFloat(lineSplit[i + 1]);
-                vertexCount += dataPerVertex;
-                break;
-            }
-            case 'vn': {
-                for (let i = 0; i < dataPerVertexNormal; i++) 
-                    vertexNormals[vertexNormalCount + i] = parseFloat(lineSplit[i + 1]);
-                vertexNormalCount += dataPerVertexNormal;
-                break;
-            }
-            case 'vt': {
-                for (let i = 0; i < dataPerTexCoord; i++) 
-                    texCoords[texCoordCount + i] = parseFloat(lineSplit[i + 1]);
-                texCoordCount += dataPerTexCoord;
-                break;
-            }
-            case 'f': {
-                for (let i = 0; i < 3; i++) 
-                {
-                    const faceSplit = lineSplit[i + 1].split('/');
-                    for (let j = 0; j < 3; j++) 
-                        faceIndices[faceIndicesCount + i * 3 + j] = parseInt(faceSplit[j]);
-                }
-                faceIndicesCount += dataPerFaceIndex;
-                break;
-            }
-        }
-    }
-
-    const cantor = (a: number, b: number) => {
-        return (a + b + 1) * (a + b) / 2 + b;
-    }
-     
-    const hash = (a: number, b: number, c: number) => {
-        return cantor(a, cantor(b, c));
-    }
-
-    const correctedIndexBuffer: Record<number, number> = { };
-    const indexBuffer = new Array<number>();
-    const vertexBuffer = new Array<number>();
-    let count = 0;
-
-    for (let i = 0; i < faceIndices.length; i += 3) {
-        const hashVal = hash(faceIndices[i],
-                             faceIndices[i + 1],
-                             faceIndices[i + 2]);
-
-        if (hashVal in correctedIndexBuffer)
-        {
-            indexBuffer.push(correctedIndexBuffer[hashVal]);
-        }
-        else
-        {
-            const vertexPosIndex = faceIndices[i] - 1;
-            vertexBuffer.push(vertexPositions[vertexPosIndex * dataPerVertex],
-                              vertexPositions[vertexPosIndex * dataPerVertex + 1],
-                              vertexPositions[vertexPosIndex * dataPerVertex + 2])
-
-            const texCoordIndex = faceIndices[i + 1] - 1;
-            vertexBuffer.push(vertexPositions[texCoordIndex * dataPerTexCoord],
-                              vertexPositions[texCoordIndex * dataPerTexCoord + 1])
-
-            const vertexNormalIndex = faceIndices[i + 2] - 1;
-            vertexBuffer.push(vertexPositions[vertexNormalIndex * dataPerVertexNormal],
-                              vertexPositions[vertexNormalIndex * dataPerVertexNormal + 1],
-                              vertexPositions[vertexNormalIndex * dataPerVertexNormal + 2])
-
-            correctedIndexBuffer[hashVal] = count;
-            indexBuffer.push(count);
-            count += 1;
-        }
-    }
-
+  export interface VaoMaterialInfo {
+    vao: WebGLVertexArrayObject;
+    iboLength: number;
+    material: Material;
+  }
+  
+  export const loadObj = (gl: WebGL2RenderingContext, objFileContent: string, mtlFileContent: string): VaoMaterialInfo[] => {
+      const materials = parseMTL(mtlFileContent);
+      let currentMaterial: string | null = null;
+  
+      const modelData: {
+          [materialName: string]: {
+              vertices: number[],
+              normals: number[],
+              texCoords: number[],
+              indices: number[]
+          }
+      } = {};
+  
+      const vertexPositions: number[] = [];
+      const vertexNormals: number[] = [];
+      const texCoords: number[] = [];
+      const tempFaces: Array<[number, number, number]> = [];
+  
+      const splitFileContent = objFileContent.split('\n');
+      for (const line of splitFileContent) {
+          const lineSplit = line.split(' ');
+          switch (lineSplit[0]) {
+              case 'usemtl':
+                  currentMaterial = lineSplit[1];
+                  if (!modelData[currentMaterial]) {
+                      modelData[currentMaterial] = { vertices: [], normals: [], texCoords: [], indices: [] };
+                  }
+                  break;
+              case 'v':
+                  vertexPositions.push(...lineSplit.slice(1).map(parseFloat));
+                  break;
+              case 'vn':
+                  vertexNormals.push(...lineSplit.slice(1).map(parseFloat));
+                  break;
+              case 'vt':
+                  texCoords.push(...lineSplit.slice(1).map(parseFloat));
+                  break;
+              case 'f':
+                  for (let i = 1; i <= 3; i++) {
+                      const [v, vt, vn] = lineSplit[i].split('/').map(x => parseInt(x) - 1);
+                      tempFaces.push([v, vt, vn]);
+                      if (currentMaterial) {
+                          modelData[currentMaterial].indices.push(tempFaces.length - 1); 
+                      }
+                  }
+                  break;
+          }
+      }
+  
+      Object.keys(modelData).forEach(material => {
+          const materialData = modelData[material];
+          materialData.indices.forEach(index => {
+              const [v, vt, vn] = tempFaces[index];
+              materialData.vertices.push(vertexPositions[3 * v], vertexPositions[3 * v + 1], vertexPositions[3 * v + 2]);
+              materialData.texCoords.push(texCoords[2 * vt], texCoords[2 * vt + 1]);
+              materialData.normals.push(vertexNormals[3 * vn], vertexNormals[3 * vn + 1], vertexNormals[3 * vn + 2]);
+          });
+      });
+      
+      const vaoInfos: VaoMaterialInfo[] = Object.keys(modelData).map(material => {
+          const data = modelData[material];
+          const vao = createVAO(gl, data.vertices, data.normals, data.texCoords, data.indices);
+          return { vao, iboLength: data.indices.length, material: materials[material] };
+        
+      });
+  
+      return vaoInfos;
+  }
+  
+function createVAO(gl: WebGL2RenderingContext, vertices: number[], normals: number[], texCoords: number[], indices: number[]): WebGLVertexArrayObject {
     const vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
 
-    const vbo = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexBuffer), gl.STATIC_DRAW);
+    const vertexArray = new Array<number>(vertices.length + normals.length + texCoords.length);
+
+    for (let index = 0; index < (vertexArray.length) / 8; index++) {
+        vertexArray[index*8] = vertices[index*3];
+        vertexArray[index*8 + 1] = vertices[index*3 + 1];
+        vertexArray[index*8 + 2] = vertices[index*3 + 2];
+
+        vertexArray[index*8 + 3] = normals[index*3];
+        vertexArray[index*8 + 4] = normals[index*3 + 1];
+        vertexArray[index*8 + 5] = normals[index*3 + 2];
+
+        vertexArray[index*8 + 6] = texCoords[index*2];
+        vertexArray[index*8 + 7] = texCoords[index*2 + 1];
+    }
+
+    // Vertex buffer
+    const vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexArray), gl.STATIC_DRAW);
     gl.enableVertexAttribArray(0);
     gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 8 * 4, 0);
+
+    // Texture coordinate buffer
     gl.enableVertexAttribArray(1);
     gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 8 * 4, 3 * 4);
+
+    // Normal buffer
     gl.enableVertexAttribArray(2);
     gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 8 * 4, 5 * 4);
 
-    const ibo = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(indexBuffer), gl.STATIC_DRAW);
+    // Index buffer
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(indices), gl.STATIC_DRAW);
 
-    let materials = parseMTL(mtlFileContent);
-
-    return {
-        vbo: vao, 
-        iboLength: indexBuffer.length,
-        materials: materials
-    }
+    return vao;
 }
+  
 
 function parseMTL(text) {
     const materials = {};
@@ -156,7 +142,7 @@ function parseMTL(text) {
         material = {};
         materials[unparsedArgs] = material;
       },
-      /* eslint brace-style:0 */
+
       Ns(parts)       { material.shininess      = parseFloat(parts[0]); },
       Ka(parts)       { material.ambient        = parts.map(parseFloat); },
       Kd(parts)       { material.diffuse        = parts.map(parseFloat); },
@@ -188,7 +174,7 @@ function parseMTL(text) {
       const parts = line.split(/\s+/).slice(1);
       const handler = keywords[keyword];
       if (!handler) {
-        console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+        console.warn('unhandled keyword:', keyword); 
         continue;
       }
       handler(parts, unparsedArgs);
